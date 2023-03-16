adccfae4a75f05329a77875d066ddea239dd12d6
Hellom38 <98119185+hellom38@users.noreply.github.com>
2023-03-16 07:39:24 +0100

    Update __main__.py

--- src/reader/__main__.py
+++ src/reader/__main__.py
@@ -1,51 +1,6 @@
-"""Read the latest Real Python tutorials.
-
-Usage:
-------
-
-    $ realpython [options] [id] [id ...]
-
-List the latest tutorials:
-
-    $ realpython
-
-Read one tutorial:
-
-    $ realpython <id>
-
-    where <id> is the number shown when listing tutorials.
-
-Read the latest tutorial:
-
-    $ realpython 0
-
-
-Available options are:
-
-    -h, --help         Show this help
-    -l, --show-links   Show links in text
-
-
-Contact:
---------
-
-- https://realpython.com/contact/
-
-More information is available at:
-
-- https://pypi.org/project/realpython-reader/
-- https://github.com/realpython/reader
-
-
-Version:
---------
-
-- realpython-reader v1.1.1
-"""
-# Standard library imports
 import sys
 
-# Reader imports
+
 import Ethack
 from Ethack import webattack,accountattack,viewer
 
