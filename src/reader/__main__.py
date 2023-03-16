938d452c24c90370ec4004be9b5ed9719aaae1c9
Hellom38 <98119185+hellom38@users.noreply.github.com>
2023-03-16 07:38:48 +0100

    Update __main__.py

--- src/reader/__main__.py
+++ src/reader/__main__.py
@@ -46,12 +46,12 @@ Version:
 import sys
 
 # Reader imports
-import reader
-from reader import feed, viewer
+import Ethack
+from Ethack import webattack,accountattack,viewer
 
 
 def main() -> None:
-    """Read the Real Python article feed."""
+    
     args = [a for a in sys.argv[1:] if not a.startswith("-")]
     opts = [o for o in sys.argv[1:] if o.startswith("-")]
 
@@ -60,23 +60,7 @@ def main() -> None:
         viewer.show(__doc__)
         raise SystemExit()
 
-    # Should links be shown in the text
-    show_links = "-l" in opts or "--show-links" in opts
-
-    # Get URL from config file
-    url = reader.URL
-
-    # An article ID is given, show article
-    if args:
-        for article_id in args:
-            article = feed.get_article(article_id, links=show_links, url=url)
-            viewer.show(article)
-
-    # No ID is given, show list of articles
-    else:
-        site = feed.get_site(url=url)
-        titles = feed.get_titles(url=url)
-        viewer.show_list(site, titles)
+  
 
 
 if __name__ == "__main__":
